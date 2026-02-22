9254f2c9b663ac93d95cea6b8ab95ad9a6e026d2
Ovsyanica <andrey-ovchinn1kov@yandex.ru>
2026-02-22 18:55:01 +0300

    FEAT add pipeline form getting targets

--- 2. Target/form_target/check_dataset.py
+++ 2. Target/form_target/check_dataset.py
@@ -1,8 +1,15 @@
 import pandas as pd
 import numpy as np
-df = pd.read_csv('btcusdt_5m_final_cleaned.csv', parse_dates=['ts'], index_col='ts')
-df.sort_index(inplace=True)
-print(f"Δt: {df.index.to_series().diff().median()}")
-print(f"Период: {df.index.min()} to {df.index.max()}")
-print(f"Средняя волатильность: {df['log_return_1min'].std()}")
-print(df.describe())
+
+
+def run(input_path: str) -> None:
+    df = pd.read_csv(input_path, parse_dates=['ts'], index_col='ts')
+    df.sort_index(inplace=True)
+    print(f"Δt: {df.index.to_series().diff().median()}")
+    print(f"Период: {df.index.min()} to {df.index.max()}")
+    print(f"Средняя волатильность: {df['log_return_1min'].std()}")
+    print(df.describe())
+
+
+if __name__ == "__main__":
+    run('btcusdt_5m_final_cleaned.csv')
